6f6671fb52ab1ed9eeb639899d245dc0a020be6d
Karissa <k@karissademi.com>
2018-12-03 20:54:43 -0500

    Rename files

---
+++ components/PhilaHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="site-header app group">
+    <div class="row expanded">
+      <div class="columns">
+        <a href="//phila.gov" class="logo">
+          <!--TODO: make local + svg -->
+          <img src="//phila.gov/media/logo/city-of-philadelphia.svg" alt="City of Philadelphia">
+        </a>
+        <div class="app-divide"></div>
+        <div class="page-title-container">
+          <a href="/">
+            <h1 class="page-title">{{ appTitle }}</h1>
+          </a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    appTitle: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ components/Question.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="question">
+    <h2>{{ question }}</h2>
+    <p class="helper-text">Your primary residence is the place where you usually live. If you receive your mail at a place or use its address for your taxes or IDs, it’s likely your primary residence.</p>
+    <input
+      id="philly"
+      v-model="residence"
+      type="radio"
+      name="residence"
+      value="philly">
+    <label
+      for="philly">Yes</label><br>
+    <input
+      id="elsewhere"
+      v-model="residence"
+      type="radio"
+      name="residence"
+      value="elsewhere">
+    <label
+      for="elsewhere">No</label>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    count () {
+      return this.$store.state.question1a
+    }
+  }
+}
+</script>
